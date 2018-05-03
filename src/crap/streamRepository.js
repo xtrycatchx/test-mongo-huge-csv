b833dd13847da87448500835ebb074c4a9582f44
Paul Sydney <xtrycatchx@gmail.com>
2018-05-03 16:50:00 +0800

    added crappy csv streaming

--- src/crap/streamRepository.js
+++ src/crap/streamRepository.js
@@ -8,29 +8,27 @@ export default class StreamRepository {
     }
 
     execute(res) {
-
-        //TODO use csv format
-        
-        res.set('Content-Type', 'application/json');
-        res.write('[');
+        res.setHeader('Content-disposition', 'attachment; filename=data.csv');
+        res.set('Content-Type', 'text/csv');
+        res.write('>>>>> HEADERS HERE');
+        res.write('\r\n')
         let prevChunk = null;
-
         const stream = db.get().collection('weird').find().stream()
         stream.on('error', err => {
             console.error(err)
         })
         stream.on('data', doc => {
-            console.log(doc)
             if (prevChunk) {
-                res.write(JSON.stringify(prevChunk) + ',');
+                res.write(Object.values(prevChunk).join(','));
+                res.write('\r\n')
             }
             prevChunk = doc;
         })
         stream.on('end', () => {
             if (prevChunk) {
-                res.write(JSON.stringify(prevChunk));
+                res.write(Object.values(prevChunk).join(','));
             }
-            res.end(']');
+            res.end('\r\n');
         });
     }
 }
